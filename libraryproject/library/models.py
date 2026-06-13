afc8380e34dac292b74155b23ccfc684bd905e87
burundicafe <mlgibbons.2@gmail.com>
2026-06-13 15:38:59 -0400

    Added Publisher field to Book model and updated index.html to display publisher information. Also created a new migration for the changes made to the models. Updated the urls.py to include the new view for displaying publisher information on the index page. Updated the .gitignore file to exclude the runDjango.py file from being tracked by git. Updated the index.html template to display the publisher information for each book in the list. This will allow users to see which publisher is associated with each book when they view the index page. Updated the design of the index page to accommodate the new publisher information, ensuring that the layout remains clean and user-friendly. This includes adding a new column for the publisher in the table that lists the books.

--- libraryproject/library/models.py
+++ libraryproject/library/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
+    publisher = models.CharField(max_length=100, blank=True, default="")
     year_published = models.IntegerField(null = False, blank = False)
     read = models.BooleanField(default=False)
 
